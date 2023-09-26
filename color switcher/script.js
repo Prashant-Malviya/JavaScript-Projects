7d9a135b4fcef08d1a699be3c3f3640e5adef89d
Prashant Malviya <prashantmalviya272002@gmail.com>
2023-09-26 23:33:47 +0530

    h1 color adjust according to body color

--- color switcher/script.js
+++ color switcher/script.js
@@ -2,18 +2,22 @@ const buttons = document.querySelectorAll('.box');
 // console.log(buttons)
 const body = document.querySelector('body');
 
+const h1 = document.querySelector('h1');
+
 buttons.forEach(function(button) {
     // console.log(button);
     button.addEventListener('click',function(e){
         // console.log(e);
         // console.log(e.target);//target will provide us the values of the particular event
         if(e.target.id === 'box1'){
+            h1.style.color = 'black';
             body.style.backgroundColor = 'yellow';
         }
         if(e.target.id === 'box2'){
             body.style.backgroundColor = 'red';
         }
         if(e.target.id === 'box3'){
+            h1.style.color = 'white';
             body.style.backgroundColor = 'black';
         }
         if(e.target.id === 'box4'){
