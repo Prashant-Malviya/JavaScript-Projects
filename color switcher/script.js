d1aed029ef1d12ad9bd00941d435029a751b078c
Prashant Malviya <prashantmalviya272002@gmail.com>
2023-09-26 23:27:18 +0530

    color switcher app built

---
+++ color switcher/script.js
@@ -0,0 +1,23 @@
+const buttons = document.querySelectorAll('.box');
+// console.log(buttons)
+const body = document.querySelector('body');
+
+buttons.forEach(function(button) {
+    // console.log(button);
+    button.addEventListener('click',function(e){
+        // console.log(e);
+        // console.log(e.target);//target will provide us the values of the particular event
+        if(e.target.id === 'box1'){
+            body.style.backgroundColor = 'yellow';
+        }
+        if(e.target.id === 'box2'){
+            body.style.backgroundColor = 'red';
+        }
+        if(e.target.id === 'box3'){
+            body.style.backgroundColor = 'black';
+        }
+        if(e.target.id === 'box4'){
+            body.style.backgroundColor = 'cyan';
+        }
+    })
+});
